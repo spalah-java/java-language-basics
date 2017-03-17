af4e9263f04e9ccc09e5b438c1b2f328b8502ee7
Kostiantyn Huliaiev <khuliaiev@playtika.com>
2017-03-17 18:18:51 +0200

    module 26 example rest

--- src/main/java/ua/spalah/spring/all/controller/CatController.java
+++ src/main/java/ua/spalah/spring/all/controller/CatController.java
@@ -3,10 +3,12 @@ package ua.spalah.spring.all.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import ua.spalah.spring.all.models.Cat;
 import ua.spalah.spring.all.services.CatService;
@@ -41,13 +43,15 @@ public class CatController {
         return "cat/edit";
     }
 
-    @RequestMapping(path = "/edit", method = RequestMethod.POST)
-    public String edit(@ModelAttribute Cat cat) {
-        if (cat.getId() != null) {
-            cat = catService.update(cat);
-        } else {
-            cat = catService.save(cat);
-        }
-        return "redirect:/cat?id=" + cat.getId();
+    @RequestMapping(path = "/json/{id}", method = RequestMethod.GET)
+    public @ResponseBody Cat edit(@PathVariable("id") long id) {
+        return catService.findById(id);
+    }
+
+    @RequestMapping(path = "/json", method = RequestMethod.POST)
+    public @ResponseBody Cat edit(@RequestBody Cat cat) {
+        return cat.getId() != null ?
+                catService.update(cat) :
+                catService.save(cat);
     }
 }
